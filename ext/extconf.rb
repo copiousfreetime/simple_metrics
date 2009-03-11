98a29d07f729c28f1c908fac1fe676287a322f68
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-03-11 08:38:40 -0600

    cleanup

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,6 +3,6 @@ create_makefile( 'simple_metrics_ext', 'src' )
 
 File.open("Makefile", "ab") do |f|
   f.puts 
-  f.puts "test_prog: $(OBJS) test/test_sm.o"
+  f.puts "demo: $(OBJS) test/test_sm.o"
   f.puts "\t$(CC) -o $@ $(OBJS) test_sm.o $(LIBPATH) $(DLDFLAGS) $(LOCAL_LIBS) $(LIBS)"
 end
