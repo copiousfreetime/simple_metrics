7427269fc840d704e3acba8444a3c0dbe3d90cfd
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-03-11 16:29:11 -0600

    extension written

---
+++ ext/src/simple_metrics_ext.c
@@ -0,0 +1,167 @@
+#include "simple_metrics_ext.h"
+
+/* classes defined here */
+VALUE mSM;         /* SimpleMetrics */
+VALUE mSM_Metric;  /* SimpleMetrics::Metric */
+VALUE mSME;        /* SimpleMetrics::Ext */
+VALUE cSME_Metric; /* SimpleMetrics::Ext::Metric */
+
+VALUE sm_free( simple_metrics* sm )
+{
+    simple_metrics_free( sm );
+    return Qnil;
+}
+
+VALUE sm_alloc( VALUE klass )
+{
+    VALUE obj;
+    simple_metrics* sm = simple_metrics_new();
+
+    obj = Data_Wrap_Struct( klass, NULL, sm_free, sm );
+
+    return obj;
+}
+
+
+VALUE sm_initialize( VALUE self, VALUE name )
+{
+
+    rb_call_super( 1, &name );
+    return self;
+}
+
+
+VALUE sm_update( VALUE self, VALUE v )
+{
+    double          new_v = NUM2DBL( v );
+    simple_metrics *sm;
+
+    Data_Get_Struct( self, simple_metrics, sm );
+
+    simple_metrics_update( sm, new_v );
+
+    return Qnil;
+}
+
+VALUE sm_count( VALUE self )
+{
+    simple_metrics *sm;
+    long            result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_count( sm );
+    r      = LONG2NUM( result );
+
+    return r;
+}
+
+VALUE sm_min( VALUE self )
+{
+    simple_metrics *sm;
+    double          result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_min( sm );
+    r      = rb_float_new(  result );
+
+    return r;
+}
+
+
+VALUE sm_max( VALUE self )
+{
+    simple_metrics *sm;
+    double          result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_max( sm );
+    r      = rb_float_new(  result );
+
+    return r;
+}
+
+VALUE sm_mean( VALUE self )
+{
+    simple_metrics *sm;
+    double          result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_mean( sm );
+    r      = rb_float_new(  result );
+
+    return r;
+}
+
+
+VALUE sm_rate( VALUE self )
+{
+    simple_metrics *sm;
+    double          result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_rate( sm );
+    r      = rb_float_new(  result );
+
+    return r;
+}
+
+
+VALUE sm_sum( VALUE self )
+{
+    simple_metrics *sm;
+    double          result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_sum( sm );
+    r      = rb_float_new(  result );
+
+    return r;
+}
+
+
+VALUE sm_stddev( VALUE self )
+{
+    simple_metrics *sm;
+    double          result;
+    VALUE           r;
+    
+    Data_Get_Struct( self, simple_metrics, sm );
+    result = simple_metrics_stddev( sm );
+    r      = rb_float_new(  result );
+
+    return r;
+}
+
+
+void Init_simple_metrics_ext()
+{
+    VALUE cSM_Metric;
+
+    mSM  = rb_define_module( "SimpleMetrics" );
+    mSME = rb_define_module_under( mSM, "Ext" );
+
+    /* load the class we inherit from */
+    rb_require("simplemetrics/metric");
+
+    cSM_Metric = rb_const_get( mSM, rb_intern( "Metric" ) );
+
+    cSME_Metric = rb_define_class_under( mSME, "Metric", cSM_Metric );
+
+    rb_define_alloc_func(cSME_Metric, sm_alloc); 
+    rb_define_method( cSME_Metric, "initialize", sm_initialize, 1 );
+    rb_define_method( cSME_Metric, "update", sm_update, 1 );
+    rb_define_method( cSME_Metric, "count", sm_count, 0 );
+    rb_define_method( cSME_Metric, "max", sm_max, 0 );
+    rb_define_method( cSME_Metric, "min", sm_min, 0 );
+    rb_define_method( cSME_Metric, "mean", sm_mean, 0 );
+    rb_define_method( cSME_Metric, "rate", sm_rate, 0 );
+    rb_define_method( cSME_Metric, "sum", sm_sum, 0 );
+    rb_define_method( cSME_Metric, "stddev", sm_stddev, 0 );
+
+}
---
+++ ext/src/simple_metrics_ext.h
@@ -0,0 +1,10 @@
+#ifndef __SIMPLE_METRICS_EXT__
+#define __SIMPLE_METRICS_EXT__
+#include "simple_metrics.h"
+
+#include <ruby.h>
+
+/* classes and modules */
+extern VALUE cSME_Metric; /* SimpleMetrics::Ext::Metric */
+
+#endif
